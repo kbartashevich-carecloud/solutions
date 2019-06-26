2c48af7c69f86b1b2a879bcb777bd2a496c584a1
Kiryl <kbartashevich@carecloud.com>
2019-06-26 05:47:30 -0400

    added 28th solution
    Signed-off-by: Kiryl <kbartashevich@carecloud.com>

---
+++ solution_28.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the quickSort function below.
+function quickSort(arr) {
+    let pivot = arr[0],
+        length = arr.length,
+        left = [],
+        right = [];
+
+    for (let i = 1; i < length; i++) {
+        if(arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+
+    return left.concat(pivot, right).join(" ");
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    let result = quickSort(arr);
+
+    ws.write(result);
+
+    ws.end();
+}
+
