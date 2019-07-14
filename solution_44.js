d8f5d799d32861225ae682b40aa34464db65cfdf
Kiryl <kbartashevich@carecloud.com>
2019-07-14 09:28:36 -0400

    added 44th solution
    Signed-off-by: Kiryl <kbartashevich@carecloud.com>

---
+++ solution_44.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the gradingStudents function below.
+ */
+function gradingStudents(grades) {
+    let newGrades = grades.map(el => {
+        if ((el < 38) || ((el % 5) === 0)) return el;
+        let iterEl = el;
+
+        while ((iterEl % 5) !== 0) {
+            ++iterEl;
+        }
+
+        if ((iterEl - el) < 3) {
+            return iterEl;
+        } else {
+            return el;
+        }
+    });
+
+    return newGrades;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let grades = [];
+
+    for (let gradesItr = 0; gradesItr < n; gradesItr++) {
+        const gradesItem = parseInt(readLine(), 10);
+        grades.push(gradesItem);
+    }
+
+    let result = gradingStudents(grades);
+
+    ws.write(result.join("\n") + "\n");
+
+    ws.end();
+}
+
